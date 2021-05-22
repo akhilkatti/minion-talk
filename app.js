7ef8ee52b9293f634e8bb4bf02a39e00c0263ffb
akhil <akhil@kali.psilent>
2021-05-22 12:52:51 +0530

    completed

--- app.js
+++ app.js
@@ -2,6 +2,28 @@ var txtInput = document.querySelector("#eng-input");
 var submitBtn = document.querySelector("#btn-submit");
 var outputTxt = document.querySelector("#output");
 
-submitBtn.addEventListener('click',() => {
-    outputTxt.textContent = txtInput.value;
-});
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text) {
+    return serverUrl + "?" + "text=" + text 
+}
+
+function errorHandler(err) {
+    console.log("error occured" + err)
+    alert("something went wrong, please try again later.")
+}
+
+function clickHandler() {
+    var input = txtInput.value
+
+    fetch(getTranslationURL(input))
+        .then(response => response.json())
+        .then(json => {
+            var translatedTxt = json.contents.translated
+            outputTxt.innerText = translatedTxt
+        })
+        .catch(errorHandler)
+}
+
+
+submitBtn.addEventListener('click', clickHandler);
